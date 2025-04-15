6d937c0c590d84a12dbf980e6be7dc4c89e5dd5e
younggg1 <1064254619@qq.com>
2025-04-15 23:34:48 +0800

    自定义hooks

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,46 +1,24 @@
 <template>
   <div class="person">
-    <h2>当前求和为：{{ sum }}</h2>
+    <h2>当前求和为：{{ sum }}，放大10倍后：{{ bigSum }}</h2>
     <button @click="add">点我sum+1</button>
+    <hr>
+    <img
+        v-for="(dog, index) in dogList"
+        :src="dog"
+        :key="index"
+    >
+    <br>
+    <button @click="getDog">再来一只小狗</button>
   </div>
 </template>
 
-<script lang="ts" setup name="Person">
-import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+<script lang="ts" setup>
+import useSum from '@/hooks/useSum'
+import useDog from '@/hooks/useDog'
 
-// 数据
-let sum = ref(0)
-// 方法
-function add() {
-  sum.value += 1
-}
-// 创建
-console.log('创建')
-
-// 挂载前
-onBeforeMount(() => {
-  // console.log('挂载前')
-})
-// 挂载完毕
-onMounted(() => {
-  console.log('子---挂载完毕')
-})
-// 更新前
-onBeforeUpdate(() => {
-  // console.log('更新前')
-})
-// 更新完毕
-onUpdated(() => {
-  // console.log('更新完毕')
-})
-// 卸载前
-onBeforeUnmount(() => {
-  // console.log('卸载前')
-})
-// 卸载完毕
-onUnmounted(() => {
-  // console.log('卸载完毕')
-})
+const { sum, add, bigSum } = useSum()
+const { dogList, getDog } = useDog()
 </script>
 
 <style scoped>
@@ -57,5 +35,10 @@ button {
 
 li {
   font-size: 20px;
+}
+
+img {
+  height: 100px;
+  margin-right: 10px;
 }
 </style>
