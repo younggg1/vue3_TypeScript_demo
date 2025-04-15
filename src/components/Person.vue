c1d4709fd96de883cfd96ae8b610ee51462c164a
younggg1 <1064254619@qq.com>
2025-04-15 20:09:23 +0800

    computed计算属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,37 +1,52 @@
 <template>
   <div class="person">
-    <h2>姓名：{{person.name}}</h2>
-    <h2>年龄：{{person.age}}，{{nl}}</h2>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
+    姓：<input
+      type="text"
+      v-model="firstName"
+  > <br>
+    名：<input
+      type="text"
+      v-model="lastName"
+  > <br>
+    <button @click="changeFullName">将全名改为li-si</button> <br>
+    全名：<span>{{ fullName }}</span> <br>
+    全名：<span>{{ fullName }}</span> <br>
+    全名：<span>{{ fullName }}</span> <br>
+    全名：<span>{{ fullName }}</span> <br>
+    全名：<span>{{ fullName }}</span> <br>
+    全名：<span>{{ fullName }}</span> <br>
   </div>
 </template>
 
-<script lang="ts" setup>
-import {reactive,toRefs,toRef} from 'vue'
+<script lang="ts" setup >
+import { ref, computed } from 'vue'
 
-// 数据
-let person = reactive({
-  name:'张三',
-  age:18
+let firstName = ref('zhang')
+let lastName = ref('san')
+
+// fullName是一个计算属性，且是只读的
+/* let fullName = computed(()=>{
+  console.log(1)
+  return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+}) */
+
+// fullName是一个计算属性，可读可写
+let fullName = computed({
+  // 当fullName被读取时，get调用
+  get() {
+    return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+  },
+  // 当fullName被修改时，set调用，且会收到修改的值
+  set(val) {
+    const [str1, str2] = val.split('-')
+    firstName.value = str1
+    lastName.value = str2
+  }
 })
 
-// 使用toRefs从person这个响应式对象中，解构出name、age，且name和age依然是响应式的
-// name和age的值是ref类型，其value值指向的是person.name和person.age
-let {name,age} = toRefs(person)
-let nl = toRef(person,'age')
-
-console.log(nl.value)
-
-// 方法
-function changeName(){
-  name.value += '~'
-  console.log(name.value,person.name)
+function changeFullName() {
+  fullName.value = 'li-si'
 }
-function changeAge(){
-  age.value += 1
-}
-
 </script>
 
 <style scoped>
@@ -41,9 +56,11 @@ function changeAge(){
   border-radius: 10px;
   padding: 20px;
 }
+
 button {
   margin: 0 5px;
 }
+
 li {
   font-size: 20px;
 }
