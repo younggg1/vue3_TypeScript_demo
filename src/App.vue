7c9cd065614a33d6fcdf9a4f35c219f71bc895f7
younggg1 <1064254619@qq.com>
2025-04-15 23:17:36 +0800

    生命周期的理解

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <!-- 务必看懂下面这一行代码 -->
-  <!-- <h2 a="1+1" :b="1+1" c="x" :d="x" ref="qwe">测试</h2> -->
-
-  <Person a="哈哈" />
+  <Person v-if="isShow"/>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup name="App">
 import Person from './components/Person.vue'
-import { reactive } from 'vue'
-import { type Persons } from './types'
+import {ref,onMounted} from 'vue'
 
-let x = 9
+let isShow = ref(true)
 
-let personList = reactive<Persons>([
-  { id: 'asudfysafd01', name: '张三', age: 18 },
-  { id: 'asudfysafd02', name: '李四', age: 20 },
-  { id: 'asudfysaf)d03', name: '王五', age: 22 }
-])
+// 挂载完毕
+onMounted(()=>{
+  console.log('父---挂载完毕')
+})
 
 </script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,37 +1,46 @@
 <template>
   <div class="person">
-    <ul>
-      <li
-          v-for="p in list"
-          :key="p.id"
-      >
-        {{ p.name }} -- {{ p.age }}
-      </li>
-    </ul>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="add">点我sum+1</button>
   </div>
 </template>
 
-<script lang="ts" setup>
-import { withDefaults } from 'vue'
-import { defineProps } from 'vue'
-import { type Persons } from '../types'
+<script lang="ts" setup name="Person">
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
 
-// 只接收list
-// defineProps(['list'])
+// 数据
+let sum = ref(0)
+// 方法
+function add() {
+  sum.value += 1
+}
+// 创建
+console.log('创建')
 
-// 接收list + 限制类型
-// defineProps<{list:Persons}>()
-
-//  接收list + 限制类型 + 限制必要性 + 指定默认值
-withDefaults(defineProps<{ list?: Persons }>(), {
-  list: () => [{ id: 'ausydgyu01', name: '康师傅·王麻子·特仑苏', age: 19 }]
+// 挂载前
+onBeforeMount(() => {
+  // console.log('挂载前')
 })
-
-
-// 接收list，同时将props保存起来
-/* let x = defineProps(['list'])
-console.log(x.list) */
-
+// 挂载完毕
+onMounted(() => {
+  console.log('子---挂载完毕')
+})
+// 更新前
+onBeforeUpdate(() => {
+  // console.log('更新前')
+})
+// 更新完毕
+onUpdated(() => {
+  // console.log('更新完毕')
+})
+// 卸载前
+onBeforeUnmount(() => {
+  // console.log('卸载前')
+})
+// 卸载完毕
+onUnmounted(() => {
+  // console.log('卸载完毕')
+})
 </script>
 
 <style scoped>
