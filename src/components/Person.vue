ae2c667b570462940c7d4fb0b81ca7ed654ce629
younggg1 <1064254619@qq.com>
2025-04-15 20:22:25 +0800

    watch监视_情况1

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,52 +1,27 @@
 <template>
   <div class="person">
-    姓：<input
-      type="text"
-      v-model="firstName"
-  > <br>
-    名：<input
-      type="text"
-      v-model="lastName"
-  > <br>
-    <button @click="changeFullName">将全名改为li-si</button> <br>
-    全名：<span>{{ fullName }}</span> <br>
-    全名：<span>{{ fullName }}</span> <br>
-    全名：<span>{{ fullName }}</span> <br>
-    全名：<span>{{ fullName }}</span> <br>
-    全名：<span>{{ fullName }}</span> <br>
-    全名：<span>{{ fullName }}</span> <br>
+    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我sum+1</button>
   </div>
 </template>
 
-<script lang="ts" setup >
-import { ref, computed } from 'vue'
-
-let firstName = ref('zhang')
-let lastName = ref('san')
-
-// fullName是一个计算属性，且是只读的
-/* let fullName = computed(()=>{
-  console.log(1)
-  return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
-}) */
-
-// fullName是一个计算属性，可读可写
-let fullName = computed({
-  // 当fullName被读取时，get调用
-  get() {
-    return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
-  },
-  // 当fullName被修改时，set调用，且会收到修改的值
-  set(val) {
-    const [str1, str2] = val.split('-')
-    firstName.value = str1
-    lastName.value = str2
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+// 数据
+let sum = ref(0)
+// 方法
+function changeSum() {
+  sum.value += 1
+}
+//监视-情况一：监视【ref】定义的【基本类型】数据
+//此处sum不需要写.value
+const stopWatch = watch(sum, (newValue, oldValue) => {
+  console.log('sum变化了', newValue, oldValue)
+  if (newValue >= 10) {
+    stopWatch()
   }
 })
-
-function changeFullName() {
-  fullName.value = 'li-si'
-}
 </script>
 
 <style scoped>
