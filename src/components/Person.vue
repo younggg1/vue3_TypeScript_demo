6abfac89e3349ce7d6d564e51da651fcfb8971fe
younggg1 <1064254619@qq.com>
2025-04-15 19:33:13 +0800

    reactive创建对象类型响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,39 +1,55 @@
 <template>
   <div class="person">
-    <h2>姓名：{{ name }}</h2>
-    <h2>年龄：{{ age }}</h2>
-    <h2>地址：{{ address }}</h2>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="showTel">查看联系方式</button>
+    <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+    <button @click="changePrice">修改汽车的价格</button>
+    <br>
+    <h2>游戏列表：</h2>
+    <ul>
+      <li
+          v-for="g in games"
+          :key="g.id"
+      >{{ g.name }}</li>
+    </ul>
+    <button @click="changeFirstGame">修改第一个游戏的名字</button>
+    <hr>
+    <h2>测试：{{ obj.a.b.c }}</h2>
+    <button @click="changeObj">测试</button>
   </div>
 </template>
 
-<script lang="ts" setup >
-import { ref } from 'vue'
-//响应式ref
-let name = ref('张三')
-let age = ref(18)
-let tel = '13888888888'
-let address = '北京昌平区宏福苑·宏福科技园'
+<script lang="ts" setup>
+import { reactive } from 'vue'
 
-console.log(1,name)
-console.log(2,age)
-console.log(3, address)
-console.log(4, tel)
+// 数据
+let car = reactive({ brand: '奔驰', price: 100 })
+let games = reactive([
+  { id: 'aysdytfsatr01', name: '王者荣耀' },
+  { id: 'aysdytfsatr02', name: '原神' },
+  { id: 'aysdytfsatr03', name: '三国志' }
+])
+let obj = reactive({
+  a: {
+    b: {
+      c: 666
+    }
+  }
+})
+
+console.log(car)
+console.log(games)
 
 // 方法
-function changeName() {
-  name.value = 'zhang-san' // JS中操作ref对象时候需要.value
-  console.log(name.value)
+function changePrice() {
+  car.price += 10
+  console.log(car.price)
 }
-function changeAge() {
-  age.value += 1
-  console.log(age.value) // JS中操作ref对象时候需要.value
+function changeFirstGame() {
+  games[0].name = '流星蝴蝶剑'
 }
-function showTel() {
-  alert(tel)
+function changeObj() {
+  obj.a.b.c = 999
 }
+
 </script>
 
 <style scoped>
@@ -46,5 +62,9 @@ function showTel() {
 
 button {
   margin: 0 5px;
+}
+
+li {
+  font-size: 20px;
 }
 </style>
