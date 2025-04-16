811884c5e8ee71580f86373aaa3961734c9e02bf
younggg1 <1064254619@qq.com>
2025-04-16 11:45:01 +0800

    路由_基本切换效果

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+// 引入路由器
+import router from './router'
 
-// 创建并挂载应用
-createApp(App).mount('#app')
+// 创建一个应用
+const app = createApp(App)
+// 使用路由器
+app.use(router)
+// 挂载整个应用到app容器中
+app.mount('#app')
