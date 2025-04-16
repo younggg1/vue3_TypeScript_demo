5d77eca6e5f5468ed44c4600505d3e338c64681d
younggg1 <1064254619@qq.com>
2025-04-16 17:42:19 +0800

    shallowRef与shallowReactive

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,70 @@
 <template>
-	<div class="container-fluid wraper">
-		<h1 class="title">
-			Vue3 组件间通信
-		</h1>
-		<hr>
-		<div class="row">
-			<div class="col-xs-3 col-md-3 col-lg-3 col-xl-3">
-				<!-- 导航区 -->
-				<router-link active-class="active" class="list-group-item" to="/props">1. props</router-link>
-				<router-link active-class="active" class="list-group-item" to="/event">2. 自定义事件</router-link>
-				<router-link active-class="active" class="list-group-item" to="/mitt">3. mitt</router-link>
-				<router-link active-class="active" class="list-group-item" to="/model">4. v-model</router-link>
-				<router-link active-class="active" class="list-group-item" to="/attrs">5. $attrs</router-link>
-				<router-link active-class="active" class="list-group-item" to="/ref-parent">6. <span class="small">$refs、$parent</span></router-link>
-				<router-link active-class="active" class="list-group-item" to="/provide-inject">7. provide、inject</router-link>
-				<router-link active-class="active" class="list-group-item" to="/pinia">8. pinia</router-link>
-				<router-link active-class="active" class="list-group-item" to="/slot">9. slot</router-link>
-			</div>
-			<div class="col-xs-9 col-md-9 col-lg-9 col-xl-9">
-				<div class="panel-body">
-					<!-- 占位一个展示区 -->
-					<router-view></router-view>
-				</div>
-			</div>
-		</div>
+	<div class="app">
+		<h2>求和为：{{ sum }}</h2>
+		<h2>名字为：{{ person.name }}</h2>
+		<h2>年龄为：{{ person.age }}</h2>
+		<h2>汽车为：{{ car }}</h2>
+		<button @click="changeSum">sum+1</button>
+		<button @click="changeName">修改名字</button>
+		<button @click="changeAge">修改年龄</button>
+		<button @click="changePerson">修改整个人</button>
+		<span>|</span>
+		<button @click="changeBrand">修改品牌</button>
+		<button @click="changeColor">修改颜色</button>
+		<button @click="changeEngine">修改发动机</button>
 	</div>
 </template>
 
-<script setup lang="ts" name="App">
+<script setup lang="ts">
+import { ref, reactive, shallowRef, shallowReactive } from 'vue'
+
+let sum = shallowRef(0)
+let person = shallowRef({
+	name: '张三',
+	age: 18
+})
+let car = shallowReactive({
+	barnd: '奔驰',
+	options: {
+		color: '红色',
+		engine: 'V8'
+	}
+})
+
+function changeSum() {
+	sum.value += 1
+}
+function changeName() {
+	person.value.name = '李四'
+}
+function changeAge() {
+	person.value.age += 1
+}
+function changePerson() {
+	person.value = { name: 'tony', age: 100 }
+}
+/* ****************** */
+function changeBrand() {
+	car.barnd = '宝马'
+}
+function changeColor() {
+	car.options.color = '紫色'
+}
+function changeEngine() {
+	car.options.engine = 'V12'
+}
+
 </script>
 
-<style>
-	.wraper .title {
-		padding: 20px;
-		text-align: center;
-		min-width: 610px;
-	}
-	.wraper .small{
-		font-size: 15px;
-	}
-	.wraper .list-group-item {
-		min-width: 230px;
-	}
+<style scoped>
+.app {
+	background-color: #ddd;
+	border-radius: 10px;
+	box-shadow: 0 0 10px;
+	padding: 10px;
+}
+
+button {
+	margin: 0 5px;
+}
 </style>
