8ece759e96e265a8e45034d736eb8d4024450b37
younggg1 <1064254619@qq.com>
2025-04-16 17:51:30 +0800

    toRaw与markRaw

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,39 @@
 <template>
 	<div class="app">
-		<h2>当前sum1为：{{ sum1 }}</h2>
-		<h2>当前sum2为：{{ sum2 }}</h2>
-		<h2>当前car1为：{{ car1 }}</h2>
-		<h2>当前car2为：{{ car2 }}</h2>
-		<button @click="changeSum1">点我sum1+1</button>
-		<button @click="changeSum2">点我sum2+1</button>
-		<button @click="changeBrand2">修改品牌(car2)</button>
-		<button @click="changeColor2">修改颜色(car2)</button>
-		<button @click="changePrice2">修改价格(car2)</button>
+		<h2>姓名：{{ person.name }}</h2>
+		<h2>年龄：{{ person.age }}</h2>
+		<button @click="person.age += 1">修改年龄</button>
+		<hr>
+		<h2>{{ car2 }}</h2>
+		<button @click="car2.price += 10">点我价格+10</button>
 	</div>
 </template>
 
-<script setup lang="ts" name="App">
-	import { ref,reactive,readonly,shallowReadonly } from "vue";
+<script setup lang="ts" >
+	import { reactive,toRaw,markRaw } from "vue";
+	import mockjs from 'mockjs'
 
-	let sum1 = ref(0)
-	let sum2 = readonly(sum1)
-	let car1 = reactive({
-		brand:'奔驰',
-		options:{
-			color:'红色',
-			price:100
-		}
+	/* toRaw */
+	let person = reactive({
+		name:'tony',
+		age:18
 	})
-	let car2 = shallowReadonly(car1)
+	// 用于获取一个响应式对象的原始对象
+	let rawPerson = toRaw(person)
+	// console.log('响应式对象',person)
+	// console.log('原始对象',rawPerson)
 
-	function changeSum1(){
-		sum1.value += 1
-	}
-	function changeSum2(){
-		sum2.value += 1 //sum2是不能修改的
-	}
 
-	function changeBrand2(){
-		car2.brand = '宝马'
-	}
-	function changeColor2(){
-		car2.options.color = '绿色'
-	}
-	function changePrice2(){
-		car2.options.price += 10
-	}
+	/* markRaw */
+	let car = markRaw({brand:'奔驰',price:100})
+	let car2 = reactive(car)
+
+	console.log(car)
+	console.log(car2)
+
+	let mockJs = markRaw(mockjs)
+
+
 </script>
 
 <style scoped>
